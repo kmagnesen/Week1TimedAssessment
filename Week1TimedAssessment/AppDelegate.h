9d19dce9f810c19709eb5b41e8ecdcdce5fe7e57
Kyle Magnesen <kylewalkermagnesen@hotmail.com>
2015-01-09 09:11:09 -0600

    Initial Commit

---
+++ Week1TimedAssessment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1TimedAssessment
+//
+//  Created by Kyle Magnesen on 1/9/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
